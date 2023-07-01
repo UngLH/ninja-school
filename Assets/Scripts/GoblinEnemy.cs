7a2989c8f5540a987bd434783574dfced0cebccc
MinhNamThan <84622781+MinhNamThan@users.noreply.github.com>
2023-07-02 04:08:38 +0700

    enemy attack player

--- Assets/Scripts/GoblinEnemy.cs
+++ Assets/Scripts/GoblinEnemy.cs
@@ -37,12 +37,20 @@ public class GoblinEnemy : MonoBehaviour
         cooldownTimer += Time.deltaTime;
 
         //Attack only when player in sight?
-        if (PlayerInSight())
+        if (PlayerInSight() && PlayerStatus.currentHealth > 0)
         {
             if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
                 anim.SetTrigger("Attack1");
+                RaycastHit2D hit = 
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
+            if(hit.collider.CompareTag("Player"))
+            {
+                hit.collider.GetComponent<PlayerStatus>().TakeDamage(damage);
+            }
                 
             }
         }
